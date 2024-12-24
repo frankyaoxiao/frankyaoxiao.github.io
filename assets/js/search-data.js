55b363ee023a76ae91bbfafe9b6a5e5de3ec93b9
frankyaoxiao <frankyaoxiao@gmail.com>
2024-12-24 05:39:59 +0000

    Deploying to gh-pages from @ frankyaoxiao/frankyaoxiao.github.io@e71c0679a9eac9874edcdd7904aae2b2271e83fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,16 +389,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-bringing-this-website-to-life",
+          title: 'Bringing this website to life!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
